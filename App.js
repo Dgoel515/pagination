93526ad222c60255878ce36143d68b1ae16703e0
Deepika goyal <86119379+Dgoel515@users.noreply.github.com>
2021-10-06 23:48:27 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import FirstComponents from './FirstComponents'
+
+import PaginationExample from './PaginationExample'
+function App() {
+  return (
+    <div className="App">
+        <FirstComponents/>
+      
+    </div>
+  );
+}
+
+export default App;
